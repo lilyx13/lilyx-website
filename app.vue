e02ea4fe0c020ce64e7eed7f0a7842809310f3da
Ashlyn <ashlyn@lilyx.io>
2021-12-31 10:17:48 -0700

    fixed yarn issues

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+<header>
+  <p>app.vue page</p>
+</header>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+  <h1>index page</h1>
+</template>
