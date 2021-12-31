f1055ab8b678e4b6cf535d6c52fe0e9a54d90b5b
Ashlyn <ashlyn@lilyx.io>
2021-12-31 11:43:49 -0700

    fixed storyblok content preview render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css'],
   buildModules: [],
+  modules: [
+    ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_API_KEY }]
+  ],
   build: {
     postcss: {
       postcssOptions: require('./postcss.config.js')
